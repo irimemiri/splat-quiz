314ad4e132756ea8f9335383a631847897d7255a
irimemiri <81759871+irimemiri@users.noreply.github.com>
2021-04-04 19:23:05 +0900

    データをテスト表示

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <table>
+      <tr>
+        <th v-for="col in columns" :key="col">{{ col }}</th>
+      </tr>
+      <tr v-for="weapon in weapons" :key="weapon.id">
+        <td>{{ weapon.id }}</td>
+        <td>{{ weapon.name }}</td>
+        <td>{{ weapon.subWeapon }}</td>
+        <td>{{ weapon.spWeapon }}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      columns: ['ID', 'ブキ', 'サブ', 'スペシャル'],
+      weapons: [
+        { id: 1, name: 'スプラシューター', subWeapon: 'スプラッシュボム', spWeapon: 'インクアーマー' },
+        { id: 2, name: 'スプラローラー', subWeapon: 'カーリングボム', spWeapon: 'スーパーチャクチ' },
+        { id: 3, name: 'スプラマニューバー', subWeapon: 'クイックボム', spWeapon: 'マルチミサイル' },
+        { id: 4, name: 'パブロ', subWeapon: 'スプラッシュボム', spWeapon: 'スーパーチャクチ' },
+        { id: 5, name: 'ボトルガイザー', subWeapon: 'スプラッシュボム', spWeapon: 'バブルランチャー' }
+      ]
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* for now */
+  width: 600px;
+  margin: 0 auto;
 }
 </style>
