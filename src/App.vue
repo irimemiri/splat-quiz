72455bee062adf1a00f995c7f68904913d78f886
irimemiri <81759871+irimemiri@users.noreply.github.com>
2021-04-04 23:41:57 +0900

    ソート実装

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
+    <p><label>ブキ名</label><input type="text" v-model="searchName"></p>
     <table>
       <tr>
-        <th v-for="col in columns" :key="col">{{ col }}</th>
+        <th v-for="(col, key) in columns" :key="col" :class="sortedClass(key)" @click="switchSort(key)">
+          {{ col }}
+        </th>
       </tr>
-      <tr v-for="weapon in weapons" :key="weapon.id">
+      <tr v-for="weapon in limited" :key="weapon.id">
         <td>{{ weapon.id }}</td>
         <td>{{ weapon.name }}</td>
         <td>{{ weapon.subWeapon }}</td>
@@ -21,14 +24,56 @@ export default {
   },
   data() {
     return {
-      columns: ['ID', 'ブキ', 'サブ', 'スペシャル'],
+      limit: 20,
+      sortKey: '',
+      isAsc: true,
+      searchName: '',
+      columns: {
+        id: 'ID',
+        name: 'ブキ',
+        subWeapon: 'サブ',
+        spWeapon: 'スペシャル'
+      },
       weapons: [
         { id: 1, name: 'スプラシューター', subWeapon: 'スプラッシュボム', spWeapon: 'インクアーマー' },
         { id: 2, name: 'スプラローラー', subWeapon: 'カーリングボム', spWeapon: 'スーパーチャクチ' },
         { id: 3, name: 'スプラマニューバー', subWeapon: 'クイックボム', spWeapon: 'マルチミサイル' },
         { id: 4, name: 'パブロ', subWeapon: 'スプラッシュボム', spWeapon: 'スーパーチャクチ' },
         { id: 5, name: 'ボトルガイザー', subWeapon: 'スプラッシュボム', spWeapon: 'バブルランチャー' }
-      ]
+      ],
+      sortOrders() {
+        let sortOrder = {}
+        this.columns.forEach(key => sortOrder[key] = 1)
+        return sortOrder
+      }
+    }
+  },
+  computed: {
+    limited() {
+      // return this.sorted.slice(0, this.limit)
+      return this.sorted
+    },
+  　sorted() {
+      const weapons = this.matched.slice()
+      const order = this.isAsc ? 'asc' : 'desc'
+      return _.orderBy(weapons, this.sortKey, order)
+    },
+    matched() {
+      return this.weapons.filter(el => el.name.indexOf(this.searchName) > -1)
+    }
+  },
+  methods: {
+    switchSort(key) {
+      if (this.sortKey === key) {
+        this.isAsc = !this.isAsc
+      } else {
+        this.isAsc = true
+        this.sortKey = key
+      }
+    },
+    sortedClass(col) {
+      let order = this.isAsc ? 'asc' : 'desc'
+      return this.sortKey === col ? `sorted ${order}` : ''
     }
   }
 }
@@ -37,7 +82,31 @@ export default {
 <style>
 #app {
   /* for now */
-  width: 600px;
+  width: 800px;
   margin: 0 auto;
+}
+
+table {
+  border-collapse: collapse;
+}
+
+th, td {
+  padding: 10px;
+}
+
+th {
+  cursor: pointer;
+}
+
+th.sorted.asc::after {
+    display: inline-block;
+    content: '▲';
+    font-size: .8em;
+}
+
+th.sorted.desc::after {
+  display: inline-block;
+  content: '▼';
+  font-size: .8em;
 }
 </style>
